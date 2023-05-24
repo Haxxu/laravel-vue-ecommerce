10d1a2e303f1247fc49a673794fd8201f2d34b0e
Minh Nguyen <nguyenngocminh190901@gmail.com>
2023-05-24 17:22:08 +0700

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const test = computed(() => store.state.test);
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <div>
+
+        <h1 class="bg-purple-500">{{ msg }}</h1>
+
+        {{  test }}
+
+
+    </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
